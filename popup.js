c6eb3ec55aa3d783d8451cecb567b4f9468d9218
zjbennett <zachjbennett@gmail.com>
2018-08-24 09:57:43 +0100

    settings
    started looking into the settings page.

--- popup.js
+++ popup.js
@@ -4,6 +4,19 @@ document.getElementById('submit').onclick = function(){
     //chrome.tabs.create({url:'https://meet.medio.link/webapp/conference=' + page + '?name=' + name + '&role=guest'});
     chrome.tabs.create({url:'https://meet.medio.link/webapp/#/?conference=' + page + '&name=' + name + '&join=1'});
 }
+document.getElementById('join').onclick = function(){
+    var myroom = document.getElementById('myroom').value;
+    var mynme = document.getElementById('myname').value;
+    chrome.tabs.create({url:'https://meet.medio.link/webapp/#/?conference=' + myroom + '&name=' + myname + '&join=1'});
+}
+
+document.getElementById('save').onclick = function(){
+    var myroom = document.getElementById('myroom').value;
+    var mynme = document.getElementById('myname').value; 
+    chrome.storage.sync.set({'value': myroom}, function(){
+        message('Settings Saved');
+    });
 
 
+}
 //join automatically link example: https://meet.medio.link/webapp/#/?conference=3996@medio.link&name=Bob&join=1 
